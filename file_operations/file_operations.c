5c29767fee05e72988ebb3156a74f979c88f0fdd
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-10-12 23:47:19 +0530

    file operation skeleton

--- file_operations/file_operations.c
+++ file_operations/file_operations.c
@@ -33,4 +33,43 @@ static struct file_operations fops=
 
 };
 
+static int etx_open(struct inode *inode, struct file *file){
+	
+	printk(KERN_INFO " Driver open function called...\n");
+	return 0;
+}
+ 
+static int etx_release(struct inode *inode,struct file *file){
+
+	printk(KERN_INFO "Driver release function called...\n");
+	return 0;
+}
+
+struct ssize_t etx_read(struct file *filp,char __user *buf, size_t len, loff_t *off){
+
+	printK(KERN_INFO "Driver read function called ...\n");
+	return 0;
+
+}
+
+struct ssize_t etx_write(struct file *filp,const char *buf,size_t len,loff_t *off){
+
+	printk(KERN_INFO "Driver write function called ...\n");
+	return len;
+}
+
+
+static int __init etx_driver_init(void)
+{
+		
+}
+
+static void __exit etx_driver_exit(void)
+{
+
+}
+
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
 
