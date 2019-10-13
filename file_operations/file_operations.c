2d15162c9d1f388c6e8bf09f2e4af2ad58b90c36
Yadav Lamichhane <omegazyadav1@gmail.com>
2019-10-13 16:54:11 +0530

    SCULL driver for experimentation

--- file_operations/file_operations.c
+++ file_operations/file_operations.c
@@ -45,14 +45,14 @@ static int etx_release(struct inode *inode,struct file *file){
 	return 0;
 }
 
-struct ssize_t etx_read(struct file *filp,char __user *buf, size_t len, loff_t *off){
+static ssize_t etx_read(struct file *filp,char __user *buf, size_t len, loff_t *off){
 
-	printK(KERN_INFO "Driver read function called ...\n");
+	printk(KERN_INFO "Driver read function called ...\n");
 	return 0;
 
 }
 
-struct ssize_t etx_write(struct file *filp,const char *buf,size_t len,loff_t *off){
+static ssize_t etx_write(struct file *filp,const char *buf,size_t len,loff_t *off){
 
 	printk(KERN_INFO "Driver write function called ...\n");
 	return len;
@@ -61,15 +61,65 @@ struct ssize_t etx_write(struct file *filp,const char *buf,size_t len,loff_t *of
 
 static int __init etx_driver_init(void)
 {
-		
+	/*Allocating a major number for the SCULL driver*/
+	
+	if((alloc_chrdev_region(&dev,0,1,"etx_Dev"))<0)	{
+
+		printk(KERN_INFO "Cannot allocate major number\n");
+		return -1;
+	}
+	printk(KERN_INFO "The Major number is :%d Minor number is :%d\n",MAJOR(dev),MINOR(dev));
+
+	/* Creating cdev structure */
+	cdev_init(&etx_cdev,&fops);
+
+	/* Adding character device to the system */
+	
+	if((cdev_add(&etx_cdev,dev,1))<0){
+
+		printk(KERN_INFO "Cannot add the device to the system. \n");
+		goto r_class;
+
+	}
+
+	/* Creating struct class */
+
+	if((dev_class=class_create(THIS_MODULE,"etx_calss"))==NULL){
+
+		printk(KERN_INFO "cannot create teh struct class \n");
+		goto r_class;
+	}
+	
+	/* Creating devie */
+
+	if((device_create(dev_class,NULL,dev,NULL,"etx_device"))==NULL)	{
+
+		printk(KERN_INFO " Cannot create the device  \n");
+		goto r_device;
+
+	}
+	printk(KERN_INFO "Device Driver Insert ... Done!!!\n ");
+	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
 }
 
 static void __exit etx_driver_exit(void)
 {
-
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev,1);
+	printk(KERN_INFO "Device Driver Remove .... Done!!!\n");
 }
-
 
 module_init(etx_driver_init);
 module_exit(etx_driver_exit);
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yadav Lamichhane <omegazyadav1@gmail.com");
+MODULE_DESCRIPTION("Experimenting with SCULL driver");
